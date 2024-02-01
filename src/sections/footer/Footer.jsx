8c181f6c73fc97fbb05c57cc6e0caa7e31c7e012
Abby <abbyhappying@gmail.com>
2024-01-31 22:55:07 -0800

    update footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <div className="container footer__container">
         <div className="footer_copyright">
-          <p>2023 Qihuan He &copy; All Rights Reserved</p>
+          <p>2024 Qihuan He &copy; All Rights Reserved</p>
         </div>
       </div>
     </footer>
